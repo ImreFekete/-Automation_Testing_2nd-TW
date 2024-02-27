deff3d212ec452798b489f7788fb3cfd00239595
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-02-27 11:36:38 +0100

    create successful log out test

---
+++ src/main/java/com/codecool/jiratests/model/LogOut.java
@@ -0,0 +1,26 @@
+package com.codecool.jiratests.model;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LogOut extends JiraSoftware {
+
+    public LogOut(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void run() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement profileMenu = driver.findElement(By.id("user-options"));
+        profileMenu.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("system")));
+        WebElement logOutButton = driver.findElement(By.id("system"));
+        logOutButton.click();
+    }
+}
